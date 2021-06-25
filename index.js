edc7f82dc6f38775362f23466e82ecd294ae482c
Gerson <gwrsonberrocal@gmail.com>
2021-06-24 20:46:37 -0500

    Libreria mongodb creada

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 
-const { config } = require('./config/index');
+const { config } = require('./config');
 const moviesApi = require('./routes/movies');
-
+app.use(express.json());
 moviesApi(app);
-
 app.listen(config.port, () => {
   console.log(
     'Servidor corriendo en el puerto http://127.0.0.1:%s',
