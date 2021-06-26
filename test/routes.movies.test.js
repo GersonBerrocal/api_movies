cd5a22b8556224ba4854f2462731cebfd0d59389
Gerson <gwrsonberrocal@gmail.com>
2021-06-25 21:11:57 -0500

    implementando test en los endpoints-rutas de la api

---
+++ test/routes.movies.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const proxyquire = require('proxyquire');
+
+const { MovieServiceMock, moviesMock } = require('../utils/mocks/movies');
+const testServer = require('../utils/testServer');
+
+describe('routes - movies', () => {
+  const route = proxyquire('../routes/movies', {
+    '../services/movies': MovieServiceMock,
+  });
+  const request = testServer(route);
+  describe('GET /movies', () => {
+    it('should respond with status 200', function (done) {
+      request.get('/api/movies').expect(200, done);
+    });
+    it('should response with the list of movies', function (done) {
+      // eslint-disable-next-line handle-callback-err
+      // eslint-disable-next-line node/handle-callback-err
+      request.get('/api/movies').end((err, res) => {
+        assert.deepEqual(res.body, {
+          data: moviesMock,
+          message: 'movies listed',
+        });
+        done();
+      });
+    });
+  });
+});
--- utils/mocks/movies.js
+++ utils/mocks/movies.js
@@ -1,150 +1,148 @@
 const moviesMock = [
   {
-    id: 'ad8e1067-701e-4f0c-a9ad-5f748723044f',
-    title: 'A True Mob Story',
-    year: 2011,
-    cover: 'http://dummyimage.com/123x227.png/cc0000/ffffff',
-    description:
-      'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.',
-    duration: 2038,
-    contentRating: 'PG-13',
-    source: 'https://abc.net.au/nullam/sit/amet/turpis/elementum.jpg',
-    tags: [
-      'Action|Comedy|Drama',
-      'Documentary',
-      'Action|Drama|War',
-      'Comedy',
-      'Comedy|Drama',
-    ],
-  },
-  {
-    id: '127def9f-2fc7-439d-bbf1-f292de39aeee',
-    title: 'The Fuller Brush Man',
-    year: 1993,
-    cover: 'http://dummyimage.com/233x247.png/cc0000/ffffff',
-    description:
-      'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.',
-    duration: 1919,
-    contentRating: 'PG-13',
-    source:
-      'http://vkontakte.ru/pede/ac/diam/cras/pellentesque/volutpat/dui.html',
-    tags: [
-      'Comedy',
-      'Comedy|Drama',
-      'Drama|Romance',
-      'Action|Crime|Thriller',
-      'Comedy|Horror',
-    ],
-  },
-  {
-    id: 'a5d8a70c-461d-44d2-ae3b-4fcab4f1e6bb',
-    title: 'Pulp Fiction',
+    id: '3ef118d9-79c1-4951-bc33-2ec87113d64e',
+    title: 'Pecker',
     year: 2007,
-    cover: 'http://dummyimage.com/141x187.png/dddddd/000000',
+    cover: 'http://dummyimage.com/232x207.png/5fa2dd/ffffff',
     description:
-      'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-    duration: 2050,
-    contentRating: 'R',
-    source:
-      'http://ifeng.com/turpis/integer/aliquet/massa/id/lobortis/convallis.xml',
-    tags: ['Documentary'],
+      'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.',
+    duration: 2630,
+    contentRating: 'NC-17',
+    source: 'https://apache.org/turpis/adipiscing/lorem/vitae/mattis.js',
+    tags: [
+      'Action|Animation|Children',
+      'Comedy|Drama|Romance',
+      'Drama|Romance',
+    ],
   },
   {
-    id: '95912698-b13b-44af-81a5-64f61d91a914',
-    title: 'Taras Bulba',
-    year: 1972,
-    cover: 'http://dummyimage.com/107x205.png/cc0000/ffffff',
+    id: '3eecae7d-dae9-4908-b353-5598a450c63c',
+    title: 'Cherry Crush',
+    year: 1999,
+    cover: 'http://dummyimage.com/125x231.png/dddddd/000000',
     description:
       'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-    duration: 2036,
+    duration: 1631,
     contentRating: 'PG',
-    source: 'http://is.gd/nulla/dapibus/dolor/vel/est/donec.xml',
-    tags: ['Comedy'],
+    source: 'http://trellian.com/lobortis/sapien/sapien/non/mi.jpg',
+    tags: ['Comedy|Drama', 'Comedy|Drama', 'Thriller'],
   },
   {
-    id: '34599384-c502-42de-8603-6b1e2b91e72f',
-    title: "Life's a Breeze",
-    year: 1996,
-    cover: 'http://dummyimage.com/155x156.png/cc0000/ffffff',
-    description:
-      'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.',
-    duration: 1926,
-    contentRating: 'NC-17',
-    source: 'http://soup.io/quam.xml',
-    tags: ['Adventure|Drama|War'],
-  },
-  {
-    id: 'e62b0402-0934-40d3-9215-229778803c13',
-    title: 'Lords of Flatbush, The',
-    year: 1997,
-    cover: 'http://dummyimage.com/129x185.png/ff4444/ffffff',
-    description:
-      'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.',
-    duration: 1950,
-    contentRating: 'PG-13',
-    source:
-      'https://blog.com/sem/mauris/laoreet/ut/rhoncus/aliquet/pulvinar.jsp',
-    tags: ['Drama', 'Drama', 'Comedy', 'Drama|Musical', 'Western'],
-  },
-  {
-    id: '3e4b45b1-8aeb-4590-81b7-26c31fe7d283',
-    title: 'Back In Action',
-    year: 2009,
-    cover: 'http://dummyimage.com/133x111.png/5fa2dd/ffffff',
+    id: 'da0afbe4-22a8-48a1-8d18-aa874b6b4f34',
+    title: 'Happy End',
+    year: 2001,
+    cover: 'http://dummyimage.com/206x132.png/cc0000/ffffff',
     description:
       'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.',
-    duration: 1914,
-    contentRating: 'G',
-    source: 'http://japanpost.jp/sit/amet/consectetuer/adipiscing/elit.xml',
+    duration: 295,
+    contentRating: 'NC-17',
+    source: 'http://mail.ru/nec/nisi.js',
+    tags: ['Comedy|Drama', 'Drama'],
+  },
+  {
+    id: '6ce6b7ee-cddb-4721-975d-44e57034571f',
+    title: 'Kiss Me Again',
+    year: 1990,
+    cover: 'http://dummyimage.com/174x119.png/dddddd/000000',
+    description:
+      'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.',
+    duration: 2843,
+    contentRating: 'PG',
+    source: 'https://yahoo.com/non/mi/integer/ac.jpg',
     tags: [
-      'Drama',
-      'Adventure|Comedy|Crime|Thriller',
-      'Drama',
-      'Action|Adventure|Children|IMAX',
+      'Comedy',
+      'Comedy',
+      'Action|Drama',
+      'Action|Adventure|Animation|Fantasy',
     ],
   },
   {
-    id: '11260156-ad3c-4752-895a-8e7ee30ba2ed',
-    title: 'War Made Easy: How Presidents & Pundits Keep Spinning Us to Death',
-    year: 1986,
-    cover: 'http://dummyimage.com/184x142.png/dddddd/000000',
+    id: '2c7ccbc6-9f83-497c-a2aa-41b8fce419b2',
+    title: 'When the Levees Broke: A Requiem in Four Acts',
+    year: 1984,
+    cover: 'http://dummyimage.com/239x242.png/dddddd/000000',
     description:
       'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-    duration: 1968,
+    duration: 2637,
     contentRating: 'R',
-    source: 'https://over-blog.com/sagittis/dui/vel.jsp',
-    tags: ['Drama|Romance'],
+    source: 'http://guardian.co.uk/nulla/suspendisse/potenti/cras/in.js',
+    tags: ['Comedy|Drama', 'Comedy|Documentary'],
   },
   {
-    id: 'e91f9fb1-6532-49b8-a0c2-edbaa1e648fa',
-    title: 'Perez Family, The',
+    id: '87b7b33c-f865-4864-8e4b-e865d87571fe',
+    title:
+      'Fog of War: Eleven Lessons from the Life of Robert S. McNamara, The',
     year: 2006,
-    cover: 'http://dummyimage.com/203x170.png/cc0000/ffffff',
+    cover: 'http://dummyimage.com/204x142.png/dddddd/000000',
     description:
-      'Sed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
-    duration: 1976,
-    contentRating: 'PG',
-    source: 'https://imdb.com/pede/lobortis/ligula.xml',
-    tags: ['Action|Adventure|Comedy', 'Crime|Mystery', 'Action|Crime|Drama'],
+      'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.',
+    duration: 589,
+    contentRating: 'NC-17',
+    source: 'https://printfriendly.com/id.aspx',
+    tags: ['Drama'],
   },
   {
-    id: '74bbb8d8-9b62-41cf-b267-55e0ef8f4b94',
-    title: 'Two Hands',
-    year: 2011,
-    cover: 'http://dummyimage.com/141x116.png/5fa2dd/ffffff',
+    id: '79a27971-b032-4b43-b4e2-aa622aa5d98b',
+    title: 'Black Dragons',
+    year: 2009,
+    cover: 'http://dummyimage.com/107x237.png/5fa2dd/ffffff',
     description:
-      'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
-    duration: 2014,
+      'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    duration: 188,
+    contentRating: 'NC-17',
+    source: 'https://hibu.com/praesent/lectus/vestibulum/quam/sapien.json',
+    tags: ['Comedy|Drama', 'Comedy|Drama|Romance', 'Comedy|Crime', 'Drama'],
+  },
+  {
+    id: '0b02c12a-e560-4d60-a6c3-8e22a4a36fa1',
+    title: 'White Night Wedding (Brúðguminn)',
+    year: 2005,
+    cover: 'http://dummyimage.com/169x188.png/dddddd/000000',
+    description:
+      'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.',
+    duration: 2051,
+    contentRating: 'G',
+    source: 'http://webeden.co.uk/ac/enim/in/tempor/turpis.json',
+    tags: ['Adventure|Animation|Children|Comedy|Drama|Musical|Romance'],
+  },
+  {
+    id: 'a811711f-0302-495a-bbbd-f8bc3314013e',
+    title: 'Bull Durham',
+    year: 1997,
+    cover: 'http://dummyimage.com/195x136.png/dddddd/000000',
+    description:
+      'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    duration: 337,
     contentRating: 'PG',
-    source: 'http://cbc.ca/maecenas/ut/massa/quis/augue/luctus.xml',
-    tags: [
-      'Crime|Drama',
-      'Drama|War',
-      'Drama',
-      'Action|Adventure|Sci-Fi',
-      'Drama',
-    ],
+    source: 'http://deliciousdays.com/ultrices/posuere/cubilia/curae.xml',
+    tags: ['Comedy|Drama|Romance'],
+  },
+  {
+    id: '39d839f2-5717-4af6-ac17-920bb8c11c99',
+    title: 'Transsiberian',
+    year: 1990,
+    cover: 'http://dummyimage.com/197x208.png/5fa2dd/ffffff',
+    description:
+      'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.',
+    duration: 1483,
+    contentRating: 'PG-13',
+    source: 'https://wikimedia.org/bibendum/morbi/non.aspx',
+    tags: ['Drama|Thriller', 'Drama|Romance'],
   },
 ];
 
-module.exports = { moviesMock };
+function filteredMoviesMock(tag) {
+  return moviesMock.filter((movie) => movie.tags.includes(tag));
+}
+
+class MovieServiceMock {
+  async getMovies() {
+    return Promise.resolve(moviesMock);
+  }
+
+  async createMovie() {
+    return Promise.resolve(moviesMock[0]);
+  }
+}
+
+module.exports = { moviesMock, MovieServiceMock, filteredMoviesMock };
